85e210e0520729a45d20dd153c9fb43bc413346f
TenoTrash <38758218+TenoTrash@users.noreply.github.com>
2026-09-13 18:21:14 -0300

    Actualiza referencias de contacto/autoría

--- Auto_AP_Reconnect_ResetCredentials_OTA.ino
+++ Auto_AP_Reconnect_ResetCredentials_OTA.ino
@@ -8,10 +8,8 @@
 
 
     Si esta información te resulta útil e interesante, invitame un cafecito!!!
-    
 
     Desde fuera de Argentina en:
-    
 
      O podés colaborar comprando algunos de los objetos creados en los tutoriales del blog:
      https://listado.mercadolibre.com.ar/_Envio_MercadoEnvios_CustId_13497891
